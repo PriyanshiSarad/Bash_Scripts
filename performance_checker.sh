a2e36b4319a3e823ac3099ea42b0a78f06237cd9
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2022-10-11 18:38:52 +0530

    Modification - performance_checker.sh

--- performance_checker.sh
+++ performance_checker.sh
@@ -9,28 +9,28 @@
 # Usage: ./performance_checker.sh
 
 # Logging the timestamp with date command 
-echo -e "1 -- This is the current timestamp of this server -->> $(date) \n" >> performance.log
+echo -e "1 -- This is the current timestamp of this server -->> $(date) \n" >> ~/performance.log
 
 
 # Logging whether internet is connected or not
 ping -c 1 google.com  &> /dev/null
 
 if [ "$?" -eq 0 ]; then
-	echo -e "2 -- Connectiviy -->> Internet Connected \n" >> performance.log
+	echo -e "2 -- Connectiviy -->> Internet Connected \n" >> ~/performance.log
 else
-	echo -e "2 -- Connectiviy -->> Internet not Connected \n" >> performance.log
+	echo -e "2 -- Connectiviy -->> Internet not Connected \n" >> ~/performance.log
 fi
 
 # Logging the Memory Usage of the server
-echo "3 -- Memory Usage:: " >> performance.log
-free -h | grep "Mem" >> performance.log 
+echo "3 -- Memory Usage:: " >> ~/performance.log
+free -h | grep "Mem" >> ~/performance.log 
 
 # Logging the Swap Usage of the server
-echo -e "\n4 -- Swap Usage:: " >> performance.log
-free -h | grep "Swap" >> performance.log 
+echo -e "\n4 -- Swap Usage:: " >> ~/performance.log
+free -h | grep "Swap" >> ~/performance.log 
 
 # Logging the Disk Usage of the server
-echo -e "\n5 -- Disk Usage:: " >> performance.log
-df -h >> performance.log 
+echo -e "\n5 -- Disk Usage:: " >> ~/performance.log
+df -h >> ~/performance.log 
 
 exit 0
